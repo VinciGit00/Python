62fd63ef9125c9a01e5bf4dcae142aa6f152a8be
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-08-12 22:46:55 +0200

    refactoring

--- gesture/functions.py
+++ gesture/functions.py
@@ -1,8 +1,7 @@
-#Functions
 import os
-import math
-import mediapipe as mp
 from PIL import Image
+from gtts import gTTS
+import mediapipe as mp
 from matplotlib import pyplot as plt
 from mediapipe.framework.formats import landmark_pb2
 
@@ -11,6 +10,28 @@ def prediction(recognizer, image):
     im.save(os.getcwd()+"/your_file.jpeg")
     image = mp.Image.create_from_file("your_file.jpeg")
     recognition_result = recognizer.recognize(image)
-    print(recognition_result)
-    return recognition_result.gestures
-    #return recognition_result.gestures[0][0].category_name
+    #Parte da fixare
+    try:
+        print(recognition_result.gestures[0][0].category_name)
+        return recognition_result.gestures[0][0].category_name
+    except:
+        return
+
+def read(gesture):
+    mytext = ""
+
+    if(gesture == "Open_Palm"):
+        mytext = 'Alexa spegni computer'
+    elif(gesture == "Thumb_Up"):
+        mytext = "Alexa spegni il display"
+    elif(gesture == "None"):
+        mytext = "Nessuna rilevazione"
+    
+    if not mytext== "":
+        language = 'it'
+        
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+        
+        myobj.save("speech.mp3")
+        
+        os.system("afplay speech.mp3")
--- gesture/script.py
+++ gesture/script.py
@@ -1,29 +1,27 @@
+# import the opencv library
 import cv2
-from functions import *
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-  
+from functions import *
+
 base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
   
-# define a video capture object
 vid = cv2.VideoCapture(0)
-  
+
 while(True):
-      
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-      
-    res = prediction(recognizer, frame)
-    print(res)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
+	
+	ret, frame = vid.read()
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+	res = prediction(recognizer, frame)
+
+	read(res)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
 # After the loop release the cap object
 vid.release()
 # Destroy all the windows
