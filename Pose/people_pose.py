6d8aa5cda9876ae431d9e82660113dc1f2a53165
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-08-21 22:29:40 +0200

    AGGIUNTO ROBA

--- Pose/people_pose.py
+++ Pose/people_pose.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import mediapipe as mp
 
 # Initialize Mediapipe components
@@ -6,15 +7,38 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 def main():
+
+    first_image = True
+    count = 0
     # Initialize webcam
     cap = cv2.VideoCapture(0)
+    cv2.resizeWindow("Resized_Window", 300, 700)
+
 
     # Initialize Mediapipe Pose model
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
+
+            if first_image:
+                count = count + 1
+                if count == 10:
+                    array = np.asarray(frame)
+                    print(array.shape)
+                    left, rigth, top, bottom = 0.95*array.shape[0] , 1.05*array.shape[0], 0.25*array.shape[1], 0
+                    cropped_image = array[left:rigth, bottom:top]
+
+                    pixel_values = np.arange(256)
+
+                    # Map the pixel values in cropped_image to their corresponding quantities in pixel_values
+                    mapped_array = pixel_values[cropped_image]
+                    #TODO: VADO AVANTI DA QUI IN POI
+                    #Qui devo cambiare il colore
+
+                    first_image = False
 
             # Flip the frame horizontally for a later selfie-view display
             frame = cv2.flip(frame, 1)
@@ -25,7 +49,7 @@ def main():
             # Process the frame with the Pose model
             results = pose.process(rgb_frame)
 
-            if results.pose_landmarks:
+            if results.pose_landmarks and count >= 10:
                 # Draw pose landmarks on the frame with red circles and black lines
                 mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                           landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=5, circle_radius=5),
