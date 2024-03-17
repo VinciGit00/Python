2e895d6c9d616b9a36f0aa23166ec38afe48707d
VinciGit00 <mvincig11@gmail.com>
2024-03-17 16:48:45 +0100

    add example for langchain

---
+++ Langchain_local_llm/local_llm.py
@@ -0,0 +1,7 @@
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+llm = Ollama(
+    model="llama2", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+)
+llm("The first man on the moon was ...")
